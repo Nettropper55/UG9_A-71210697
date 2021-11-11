b0760580b216fb3dc4efa47d875f240d61167650
nama singkat AndaNettropper55 <albertus.stefan@ti.ukdw.ac.id>
2021-11-11 16:46:18 +0700

    3_A_71210697.py

---
+++ 3_A_71210697.py
@@ -0,0 +1,10 @@
+#daftar belanja Doni
+
+#Input
+a = str(input("Masukan daftar belanja 1 : "))
+b = str(input("Masukan daftar belanja 2 : "))
+v = str(input("Tambahkan data ke daftar belanja 1 : "))
+w = str(input("Tambahkan data ke daftar belanja 2 : "))
+#print
+print("Daftar belanjaan 1 adalah ",a,",",v,sep="")
+print("Daftar belanjaan 2 adalah ",b,",",w,sep="")
