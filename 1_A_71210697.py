49d02d143b5ea71b67ff60d1b8673739ceb042aa
nama singkat AndaNettropper55 <albertus.stefan@ti.ukdw.ac.id>
2021-11-11 16:45:11 +0700

    1_A_71210697.py

---
+++ 1_A_71210697.py
@@ -0,0 +1,13 @@
+#menghitung keliling lingkaran
+#phi, D
+
+#input
+D = int(input("Masukan diameter : "))
+
+#proses
+Keliling = round(3.14 * D, 2)
+
+#print
+print ("keliling lingkaran adalah : ", Keliling)
+
+
